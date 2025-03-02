03a1e6027cacc1ae95b307482eda3c8800e8cb4a
AbdulSarangaa <abdulsaranga23@gmail.com>
2025-03-02 17:29:56 +0200

    feat: created costumer entity

--- cms-backend/src/main/java/com/abu/cms/models/Costumer.java
+++ cms-backend/src/main/java/com/abu/cms/models/Costumer.java
@@ -1,15 +1,30 @@
 package com.abu.cms.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_costumers")
 public class Costumer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "first_name", length = 40, nullable = false)
     private String firstName;
 
+    @Column(name = "last_name", length = 40, nullable = false)
     private String lastName;
 
+    @Column(length = 60, nullable = false, unique = true)
     private String email;
 
+    @Column(name = "cell_phone", length = 20, nullable = false, unique = true)
     private String cellPhone;
 
     public Costumer() {
