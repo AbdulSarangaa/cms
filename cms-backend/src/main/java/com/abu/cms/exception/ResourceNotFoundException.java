f0473588114663b257b3918698c741fa3c1e33c5
AbdulSarangaa <abdulsaranga23@gmail.com>
2025-03-02 17:35:03 +0200

    feat: added find costumer by id method

---
+++ cms-backend/src/main/java/com/abu/cms/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.abu.cms.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
--- cms-backend/src/main/java/com/abu/cms/services/CostumerService.java
+++ cms-backend/src/main/java/com/abu/cms/services/CostumerService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.abu.cms.exception.ResourceNotFoundException;
 import com.abu.cms.models.Costumer;
 import com.abu.cms.repositories.CostumerRepository;
 
@@ -14,8 +15,15 @@ public class CostumerService {
     @Autowired
     private CostumerRepository repository;
 
-    public List<Costumer> findAll() {
+    public List<Costumer> findAllCostumers() {
         return repository.findAll();
     }
 
+    public Costumer findCostumerById(Long costumerId) {
+        return repository.findById(costumerId).orElseThrow(() -> new ResourceNotFoundException("Costumer not found"));
+    }
+
+    public Costumer newCostumer(Costumer costumer) {
+        return repository.save(costumer);
+    }
 }
